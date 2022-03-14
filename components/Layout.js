0b62c6f3891168f1830d0f5b0fb227431d32ccc8
Hassan Ali <hassan.ali@g.austincc.edu>
2022-03-14 00:15:10 -0500

    display Product

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import Head from "next/head";
+import useStyles from "../utils/styles";
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Ushop</title>
+      </Head>
+      <AppBar position='static' className={classes.navbar}>
+        <Toolbar>
+          <Typography>Ushop</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>© {new Date().getFullYear()} UShop. All rights </Typography>
+      </footer>
+    </div>
+  );
+}
