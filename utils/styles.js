705189dad99334defda42801bf0293584e4d9c78
Hassan Ali <hassan.ali@g.austincc.edu>
2022-03-17 16:19:01 -0500

    page details added

--- utils/styles.js
+++ utils/styles.js
@@ -13,7 +13,12 @@ const useStyles = makeStyles({
     minHeight: "80vh",
   },
   footer: {
+    marginTop: 10,
     textAlign: "center",
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 export default useStyles;
