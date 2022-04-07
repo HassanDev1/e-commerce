d5f7fa19480de56cebc1c8c06fefa0b471cc5490
Jlott26016 <lott26016@gmail.com>
2022-04-07 10:43:05 -0500

    first commit of admin_dashboard2

---
+++ pages/api/admin/summary.js
@@ -0,0 +1,37 @@
+import nc from 'next-connect';
+//import Order from '../../../models/Order';
+//import Product from '../../../models/Product';
+//import User from '../../../models/User';
+import { isAuth } from '../../../utils/auth';
+import { onError } from '../../../utils/error';
+import { connectToDatabase } from '../../../utils/db';
+import { OrderedBulkOperation } from 'mongodb';
+
+//This
+const handler = nc({
+  onError,
+});
+handler.use(isAuth);
+
+//Need to implement models for (order, product, and user) to finish API
+//imports are commented out
+
+handler.get(async (req, res) => {
+  const { db } = await connectToDatabase();
+  //const orders = await Order.find({ user: req.user_id });
+  const ordersCount = await Order.countDocuments();
+  const productsCount = await Product.countDocuments();
+  const usersCount = await User.countDocuments();
+  const ordersPriceGroup = await Order.aggregate([
+    {
+      $group: {
+        _id: null,
+        sales:{$sum: '$totalPrice'}
+      }
+    }
+  ]);
+  const ordersPrice = ordersPriceGroup.length > 0 ? ordersPriceGroup[0].sales: 0;
+  res.send({ ordersCount, productsCount, usersCount, ordersPrice });
+});
+
+export default handler;
