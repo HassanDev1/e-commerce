f37dbe719549cf36f86ff2613d389b26666e8fcb
Jlott26016 <lott26016@gmail.com>
2022-04-07 13:13:20 -0500

    hopefully final commit

--- pages/api/admin/summary.js
+++ pages/api/admin/summary.js
@@ -1,36 +1,30 @@
-import nc from 'next-connect';
-//import Order from '../../../models/Order';
-//import Product from '../../../models/Product';
-//import User from '../../../models/User';
-import { isAuth } from '../../../utils/auth';
-import { onError } from '../../../utils/error';
-import { connectToDatabase } from '../../../utils/db';
+import nc from "next-connect";
+import { isAuth } from "../../../utils/auth";
+import { onError } from "../../../utils/error";
+import { connectToDatabase } from "../../../utils/db";
 
-//This
 const handler = nc({
-  onError,
+onError,
 });
 handler.use(isAuth);
 
-//Need to implement models for (order, product, and user) to finish API
-//imports are commented out
-
 handler.get(async (req, res) => {
-  const { db } = await connectToDatabase();
-  //const orders = await Order.find({ user: req.user_id });
-  const ordersCount = await Order.countDocuments();
-  const productsCount = await Product.countDocuments();
-  const usersCount = await User.countDocuments();
-  const ordersPriceGroup = await Order.aggregate([
-    {
-      $group: {
-        _id: null,
-        sales:{$sum: '$totalPrice'}
-      }
-    }
-  ]);
-  const ordersPrice = ordersPriceGroup.length > 0 ? ordersPriceGroup[0].sales: 0;
-  res.send({ ordersCount, productsCount, usersCount, ordersPrice });
+const { db } = await connectToDatabase();
+//const orders = await Order.find({ user: req.user_id });
+const ordersCount = await db.collection("Orders").countDocuments();
+const productsCount = await db.collection("Products").countDocuments();
+const usersCount = await db.collection("Users").countDocuments();
+const ordersPriceGroup = await db.collection("Orders").aggregate([
+{
+$group: {
+_id: null,
+sales: { $sum: "$totalPrice" },
+},
+},
+]);
+const ordersPrice =
+ordersPriceGroup.length > 0 ? ordersPriceGroup[0].sales : 0;
+res.send({ ordersCount, productsCount, usersCount, ordersPrice });
 });
 
 export default handler;
