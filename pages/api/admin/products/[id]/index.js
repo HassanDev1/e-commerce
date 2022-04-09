3794e9a0dcf593f7d27e6989021c94ebbf75d220
Megan Garza <Garza.Megan18@Gmail.com>
2022-04-09 15:26:05 -0500

    newest updates

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -6,6 +6,26 @@ import { ObjectId } from 'mongodb';
 const handler = nc();
 handler.use(isAuth, isAdmin);
 
+handler.post(async (req, res) =>{
+  await db.connect();
+  const newProduct = new Product({
+    name: 'sample name',
+    slug: 'sample-slug-' + Math.random(),
+    image: '/image/shirt1.jpg',
+    price: 0,
+    category: 'sample category',
+    brand: 'sample brand',
+    countInStock: 0,
+    description: 'sample description',
+    rating: 0,
+    numReview: 0,
+  });
+
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: 'Product Created', product });
+});
+
 handler.get(async (req, res) => {
   const { db } = await connectToDatabase();
   const product = await db
